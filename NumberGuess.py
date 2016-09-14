92e1a4e4d9869f477dcb62f2b1805f82d6e756fd
Andrew Gale <andrewgale1@users.noreply.github.com>
2016-09-14 11:49:47 -0700

    added more depth to game
    trying to make the user/computer interaction more like a human/human interaction. made it so the computer won't really let you quit but keeps making you play.

--- NumberGuess.py
+++ NumberGuess.py
@@ -1,36 +1,61 @@
 import random
 
-def start():
+def play():
+    print "Being a computer is pretty boring..."
+    choice = raw_input("would you like to play a game with me?  y/n ")
+
+    if choice == "y":
+        print "Yeah!"
+        guess()
+
+    if choice == "n":
+        print ""
+        print "OK... I don't understand why you'd rather have an unremarkable day.."
+        print "..not playing with a sexy computer...but I wish you well."
+        print "Actually...why don't you just play once...you might like it!"
+        print ""
+        guess()
+
+    elif choice != "y"+"n":
+        choice = raw_input("Please choose either y or n. y/n ")
+        
+
+def guess():   
     print "I am thinking of a number between 1 and 10..."
     choice = raw_input("Would you like to guess? y/n ")
 
     if choice == "y":
+        print ""
         print "Yeah!"
         begin()
 
     if choice == "n":
-        print "Bummer...bye then."
+        print "Ahh, you should give it a go, good things can happen!"
+        guess()
 
     elif choice != "y"+"n":
-        choice = raw_input("Please choose either y or n. y/n ") 
+        choice = raw_input("Please choose either y or n. y/n ")
+        
 
 def begin():
     from random import randint
     
     randomNumber = randint(1,10)
-    guess = raw_input("OK, I have my number, please guess now: 1-10 ")
+    guess = raw_input("OK, I have my number...please guess now: 1-10 ?  ")
 
     if guess == str(randomNumber):
         print "You got it! The number was " + str(randomNumber)
         choice = raw_input("Want to play again? y/n ")
 
         if choice == "y":
+            print ""
             print "Woohoo!"
             begin()
 
         if choice == "n":
-            print "Thanks for playing."
-            exit()
+            print ""
+            print "Thanks for playing...try one more time, you got this!"
+            begin()
 
         elif choice != "y"+"n":
             choice = raw_input("Please choose either y or n. y/n ")
@@ -41,14 +66,16 @@ def begin():
         choice = raw_input("Want to play again? y/n ")
 
         if choice == "y":
+            print ""
             print "Woohoo!"
             begin()
 
         if choice == "n":
-            print "Thanks for playing."
-            exit()
+            print ""
+            print "You're the best, here we go again!!!"
+            begin()
 
         elif choice != "y"+"n":
             choice = raw_input("Please choose either y or n. y/n ")        
 
-start()
+play()
